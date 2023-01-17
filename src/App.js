1e8f309e728f472c5d9de3eb85fd191f3ca425d3
Manish-choubey <109017549+Manish-choubey@users.noreply.github.com>
2023-01-17 18:28:52 +0530

    day1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Search.css";
+import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
+import { Button } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt="logo"
+        />
+       
+    
+    <form className="search">
+    
+      <div className="search__input">
+      
+        <SearchIcon className="search__inputIcon" />
+        <input />
+        <MicIcon />
+      </div>
+
+      <div className="search__buttons">
+        <Button variant="outlined" type="submit">Google Search</Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
+      </div>
+    </form>
     </div>
   );
 }
 
-export default App;
+export default App
